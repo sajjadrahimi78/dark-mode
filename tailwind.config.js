32f5e9e5545efef72f8c9fdb9192e260789afc3e
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-05-16 11:45:09 +0330

    tailwind confige

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}" , "./src/**/*.{css,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
